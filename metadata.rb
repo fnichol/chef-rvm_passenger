ef5d5e1cfeec024ea0428c80476c83124bc5207d
Fletcher Nichol <fnichol@nichol.ca>
2010-12-12 00:21:20 -0700

    Bump passenger default version to 3.0.1.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ops@opscode.com"
 license          "Apache 2.0"
 description      "Installs and configures Passenger under RVM with Apache or nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.5.0"
+version          "0.5.1"
 
 recipe "rvm_passenger", "Installs Passenger gem with RVM"
 recipe "rvm_passenger::apache2", "Enables Apache module configuration for passenger under RVM"
@@ -20,7 +20,7 @@ end
 attribute "rvm_passenger/version",
   :display_name => "Version of passenger",
   :description => "The version of passenger to deploy. This will be in a format used by the gem command.",
-  :default => "3.0.0"
+  :default => "3.0.1"
 
 attribute "rvm_passenger/rvm_ruby",
   :display_name => "RVM ruby to use",
