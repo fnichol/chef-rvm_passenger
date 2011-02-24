a7fbca35dc5ff375790281abb0e8bb7ab401c0f8
Fletcher Nichol <fnichol@nichol.ca>
2011-02-24 10:26:06 -0700

    Update maintainer details in metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Opscode, Inc."
-maintainer_email "ops@opscode.com"
+maintainer       "Fletcher Nichol"
+maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Passenger under RVM with Apache or nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
