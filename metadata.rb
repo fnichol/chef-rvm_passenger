d037f20704e51cafb4ed46f8f91103c4b9e95335
Phil Cohen <github@phlippers.net>
2011-07-28 15:35:20 -0700

    fix typo that crashes chef 0.10.x

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Passenger under RVM with Apache or nginx"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.8.0"
 
 recipe "rvm_passenger", "Installs Passenger gem with RVM"
