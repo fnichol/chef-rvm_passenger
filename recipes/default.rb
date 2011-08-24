b54b77dcab9b518832384ee207d46debd3ab9bf2
Fletcher Nichol <fnichol@nichol.ca>
2011-08-24 10:45:01 -0600

    Cookbook requires recipe[rvm::system] to track rvm cookbook updates.

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "rvm"
+include_recipe "rvm::system"
 
 if platform?("suse")
   package "libcurl-devel"
