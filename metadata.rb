f41c5bfe85c9efe78e80cc85dce92303193d2e01
Fletcher Nichol <fnichol@nichol.ca>
2012-05-31 21:10:52 -0600

    Support rvm cookbook >= 0.8.0.

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ recipe "rvm_passenger",           "Installs Passenger gem with RVM"
 recipe "rvm_passenger::apache2",  "Enables Apache module configuration for passenger under RVM"
 recipe "rvm_passenger::nginx",    "Installs Passenger gem w/ RVM, and recompiles support into Nginx"
 
-depends "rvm",      "~> 0.8.0"
+depends "rvm",      ">= 0.8.0"
 depends "nginx",    "~> 0.101.0"
 depends "apache2",  "~> 1.1.8"
 
