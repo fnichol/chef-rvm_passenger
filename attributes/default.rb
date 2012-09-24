634391ba53a3c62f84b309057122e330ac74a69a
Pavel Forkert <fxposter@gmail.com>
2012-09-24 14:37:07 +0300

    Do not set required packages via node.set, set them as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,18 +28,18 @@ default['rvm_passenger']['rvm_ruby']     = nil
 
 case platform
 when "suse"
-  node.set['rvm_passenger']['common_pkgs']  = %w{libcurl-devel}
-  node.set['rvm_passenger']['apache2_pkgs'] =
+  default['rvm_passenger']['common_pkgs']  = %w{libcurl-devel}
+  default['rvm_passenger']['apache2_pkgs'] =
     %w{apache2-devel libapr1-devel libapr-util1-devel}
 when "centos","redhat","fedora","scientific","amazon"
   if node['platform_version'].to_f < 6
-    node.set['rvm_passenger']['common_pkgs']  = %w{libcurl-devel openssl-devl zlib-devel}
+    default['rvm_passenger']['common_pkgs']  = %w{libcurl-devel openssl-devl zlib-devel}
   else
-    node.set['rvm_passenger']['common_pkgs']  = %w{curl-devel}
+    default['rvm_passenger']['common_pkgs']  = %w{curl-devel}
   end
-  node.set['rvm_passenger']['apache2_pkgs'] =  %w{httpd-devel}
+  default['rvm_passenger']['apache2_pkgs'] =  %w{httpd-devel}
 else
-  node.set['rvm_passenger']['common_pkgs']  = %w{libcurl4-openssl-dev}
-  node.set['rvm_passenger']['apache2_pkgs'] =
+  default['rvm_passenger']['common_pkgs']  = %w{libcurl4-openssl-dev}
+  default['rvm_passenger']['apache2_pkgs'] =
     %w{apache2-threaded-dev libapr1-dev libaprutil1-dev}
 end
